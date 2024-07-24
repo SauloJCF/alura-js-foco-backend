9b765ef79cf47016af80cb75d2c5eee75ffee4ef
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-07-24 19:17:02 -0300

    Reestruturação pastas, aula sobre boolean

---
+++ aula-03/tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Felipe';
+const aprovado = false;
+
+if (aprovado) {
+    console.log('Parabéns pela aprovação!');
+} else {
+    console.log('Deu ruim parceiro.');
+}
+
+if (estudante === 'Joao') {
+    console.log('Olá João!');
+} else {
+    console.log('Intruso detectado!');
+}
---
+++ aula-03/tipo-string.js
@@ -0,0 +1,18 @@
+const estudante = 'Carolina';
+const docente = 'Ana';
+
+console.log(estudante + ' ' + docente);
+
+const cumprimento = 'nosso lema é "estudar bastante"';
+
+console.log(cumprimento);
+
+const fala = `então, ele disse: "Faz o L"`;
+
+console.log(fala);
+
+console.log(`a estudante se chama ${estudante}`);
+
+const senha = ('senhaSegura1234' + estudante).toUpperCase();
+
+console.log(senha);
