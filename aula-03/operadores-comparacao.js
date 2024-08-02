6566c6f8fa83ba2700bc3632b58839a9b72fb9b5
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-08-01 21:33:48 -0300

    Operadores de comparação

---
+++ aula-03/operadores-comparacao.js
@@ -0,0 +1,13 @@
+const estaAprovado = true;
+
+if (0 == '0') {
+    console.log('Comparação foi verdadeira.');
+} else {
+    console.log('Comparação foi falsa.');
+}
+
+if (0 === '0') {
+    console.log('Comparação foi verdadeira.');
+} else {
+    console.log('Comparação foi falsa.');
+}
