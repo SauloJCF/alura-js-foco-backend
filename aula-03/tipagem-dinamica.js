25f35ae2fbe4c299e00cd4782ae519c4298108ed
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-08-01 21:35:28 -0300

    Tipagem dinâmica

---
+++ aula-03/tipagem-dinamica.js
@@ -0,0 +1,11 @@
+let minhaVariavel = 2;
+
+console.log(`"${typeof minhaVariavel}": "${minhaVariavel}"`);
+
+minhaVariavel = 'Hello world!';
+
+console.log(`"${typeof minhaVariavel}": "${minhaVariavel}"`);
+
+minhaVariavel = false;
+
+console.log(`"${typeof minhaVariavel}": "${minhaVariavel}"`);
