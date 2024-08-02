188a6c6d9f0d8820efb5de144f2619c9f2f7f8b6
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-08-01 21:31:46 -0300

    Tipo undefined

---
+++ aula-03/tipo-null-undefined.js
@@ -0,0 +1,17 @@
+let nomeEstudante;
+let telefoneEstudante;
+
+telefoneEstudante = null;
+
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+
+console.log(telefoneEstudante);
+console.log(typeof telefoneEstudante);
+
+console.log(nomeEstudante + 3);
+console.log(typeof (nomeEstudante + 3));
+
+
+console.log(telefoneEstudante + 3);
+console.log(typeof (telefoneEstudante + 3));
