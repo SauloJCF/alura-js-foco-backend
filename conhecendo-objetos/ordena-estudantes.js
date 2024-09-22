fda6a9fcb8671bca6ed58825e33c67dfc77a0483
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-09-22 12:28:59 -0300

    Ordenar estudantes

---
+++ conhecendo-objetos/ordena-estudantes.js
@@ -0,0 +1,13 @@
+const estudantes = require('./json/estudantes.json');
+
+function ordena(lista, propriedade) {
+    return lista.sort((a, b) => {
+        if (a[propriedade] < b[propriedade]) return -1;
+        else if (a[propriedade] > b[propriedade]) return 1;
+        else return 0;
+    });
+}
+
+const listaOrdenada = ordena(estudantes, 'nome');
+
+console.log(listaOrdenada);
