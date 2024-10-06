53edd172242f25a53c39c6a942de8b45c1afd773
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-10-06 11:15:32 -0300

    Implementação commander

--- primeira-biblioteca/src/cli.js
+++ primeira-biblioteca/src/cli.js
@@ -1,25 +1,46 @@
 import fs from 'fs';
+import path from 'path';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helper.js';
+import { Command } from 'commander';
 
-const argumentoCaminhoArquivo = 2;
-const argumentoCaminhoArquivoDestino = 3;
+const program = new Command();
 
-const caminhoArquivo = process.argv[argumentoCaminhoArquivo];
-const caminhoDestino = process.argv[argumentoCaminhoArquivoDestino];
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+    .action((options) => {
+        const { texto, destino } = options;
 
-fs.readFile(caminhoArquivo, 'utf-8', (erro, texto) => {
-    try {
-        if (erro) {
-            throw erro;
+        if (!texto || !destino) {
+            console.error('erro: favor inserir caminho de origem e destino');
+            program.help();
+            return;
         }
-        const resultado = contaPalavras(texto);
-        criaESalvaArquivo(resultado, caminhoDestino);
-    } catch (erro) {
-        console.log(trataErros(erro));
-    }
-});
+
+        const caminhoArquivo = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        processaArquivo(caminhoArquivo, caminhoDestino);
+    });
+
+program.parse();
+
+function processaArquivo(caminhoArquivo, caminhoDestino) {
+    fs.readFile(caminhoArquivo, 'utf-8', (erro, texto) => {
+        try {
+            if (erro) {
+                throw erro;
+            }
+            const resultado = contaPalavras(texto);
+            criaESalvaArquivo(resultado, caminhoDestino);
+        } catch (erro) {
+            console.log(trataErros(erro));
+        }
+    });
+}
 
 async function criaESalvaArquivo(listaPalavras, caminhoDestino) {
     const novoArquivo = caminhoDestino + '/resultado.txt';
