58944037239f405684a9c959db34b98107e734f0
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-10-20 12:10:04 -0300

    Herança

---
+++ orientacao-a-objetos/Admin.js
@@ -0,0 +1,13 @@
+import User from "./User.js";
+
+export default class Admin extends User {
+    constructor(nome, email, nascimento, role = 'admin', ativo = true) {
+        super(nome, email, nascimento, role, ativo);
+    }
+}
+
+const admin = new Admin('Rodrigo', 'r@r.com', '2024-01-01');
+
+console.log(admin);
+
+console.log(admin.exibeInfos());
